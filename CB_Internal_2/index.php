b160700c631cdc49a13bd57320b81693bcabba36
mlundyCCG <marcus.lundy@cloudburstgroup.com>
2024-06-02 13:42:20 -0400

    Add files via upload

---
+++ CB_Internal_2/index.php
@@ -0,0 +1,146 @@
+<?php include "./includes/header.php" ?>
+
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+  <div class="container-fluid nav-display">
+    <img src="/imgs/1cb_logo.png">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link 2</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container jumbotron-content">
+    <h1 class="display-4">Welcome To <br /> OneCloudburst</h1>
+    <p class="lead">The Central Hub for Cloudburst Resources</p>
+
+    <form class="d-flex jumbotron-search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="jumbotron-search-btn" type="submit">Search</button>
+    </form>
+
+    <button type="button" class="btn btn-light-blue"><a class="bi-house-fill dark-link-style" href="http://www.cloudburstgroup.com" target="_blank">&nbsp;    Cloudburst Main Site</a></button>
+    <button type="button" class="btn btn-light"><a class="bi-house-fill dark-link-style" href="http://www.cloudburstgroup.com" target="_blank">&nbsp;    Another Button</a></button>
+
+    <p><a href="#start"><i class="bi bi-arrow-down-square" title="See more"></i></a></p>
+  </div>
+</div>
+
+<div class="container text-center">
+  <div class="row">
+    <!-- <div class="col-12">
+      <h2 class="fs-2 subhead">Column</h2>
+    </div> -->
+  <div class="row">
+    <div class="col-3">
+      <div class="card">
+        <i class="bi bi-folder2-open card-icon"></i>
+
+        <div class="card-body">
+          <h5 class="card-title">HR Policies &amp; Forms</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>          
+    </div>
+    <div class="col-3">
+      <div class="card">
+        <i class="bi bi-laptop card-icon"></i>
+
+        <div class="card-body">
+          <h5 class="card-title">Information Technology</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>          
+    </div>
+    <div class="col-3">
+      <div class="card">
+        <i class="bi bi-calendar card-icon"></i>
+
+        <div class="card-body">
+          <h5 class="card-title">Calendar</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>          
+    </div>
+    <div class="col-3">
+      <div class="card">
+        <i class="bi bi-currency-dollar card-icon"></i>
+
+        <div class="card-body">
+          <h5 class="card-title">Accounting &amp; Finance</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>          
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-3">
+      <div class="card">
+        <i class="bi bi-folder2-open card-icon"></i>
+
+        <div class="card-body">
+          <h5 class="card-title">HR Policies &amp; Forms</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>          
+    </div>
+    <div class="col-3">
+      <div class="card">
+        <i class="bi bi-laptop card-icon"></i>
+
+        <div class="card-body">
+          <h5 class="card-title">Information Technology</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>          
+    </div>
+    <div class="col-3">
+      <div class="card">
+        <i class="bi bi-calendar card-icon"></i>
+
+        <div class="card-body">
+          <h5 class="card-title">Calendar</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>          
+    </div>
+    <div class="col-3">
+      <div class="card">
+        <i class="bi bi-currency-dollar card-icon"></i>
+
+        <div class="card-body">
+          <h5 class="card-title">Accounting &amp; Finance</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>          
+    </div>
+  </div>
+</div>
+
+<p>k</p>
+<p>k</p>
+<p>k</p>
+<p>k</p>
+<p>k</p>
+<p>k</p>
+<p>k</p>
+<p>k</p>
+<p>k</p>
+<p>k</p>
+<p>k</p>
+
+
+<?php include "./includes/footer.php" ?>
